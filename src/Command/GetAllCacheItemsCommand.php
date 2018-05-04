5c2b98a4d27c1bb1224143260b939c063ddbc5dc
Kostas Loupasakis <kloupasakis@trading-point.com>
2018-05-04 11:30:14 +0300

    Created commands that replicate the caching issue

---
+++ src/Command/GetAllCacheItemsCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Command;
+
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Cache\Adapter\TagAwareAdapter;
+use Symfony\Component\Cache\Adapter\TagAwareAdapterInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GetAllCacheItemsCommand extends ContainerAwareCommand
+{
+    /**
+     * @var TagAwareAdapterInterface
+     */
+    private $cachePool;
+
+    protected function configure()
+    {
+        $this
+            ->setName('app:get-all')
+            ->setDescription('Populates, invalidates by tag and returns tagged cases');
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->cachePool = $this->getContainer()->get('app.cache');
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $howMany = 5;
+
+        for ($i = 0; $i < $howMany; $i++) {
+            $cacheItem = $this->cachePool->getItem("item-$i");
+            $hit = $cacheItem->isHit()?'true':'false';
+            $output->writeln("{$cacheItem->getKey()}, isHit:{$hit}");
+        }
+
+    }
+}
